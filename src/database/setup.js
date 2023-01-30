adf6228a3ac7831257bc5c5c6fe9579f5c2576f3
Simenf05 <106535609+Simenf05@users.noreply.github.com>
2023-01-30 22:54:39 +0100

    unclean data

--- src/database/setup.js
+++ src/database/setup.js
@@ -1,1 +1,25 @@
-db = db.getSiblingDB('sample_db');
+const fs = require('fs');
+
+const data = fs.readFileSync("data.csv", {encoding: 'utf-8'});
+const lines = data.split('\r\n').slice(0, -1);
+
+const names = [...lines[0].split(',')]
+
+db = db.getSiblingDB('admin');
+db.auth(process.env.MONGO_INITDB_ROOT_USERNAME, process.env.MONGO_INITDB_ROOT_PASSWORD);
+db = db.getSiblingDB('barcode-scanning');
+db.createCollection('items');
+
+db.items.insertOne({"hei": "hei"});
+
+lines.slice(1).forEach(line => {
+    let doc = {};
+
+    line.split(',').forEach((val, i) => {
+        doc = Object.assign(doc, {[names[i]]: val});
+    });
+
+    /* this needs to be changed to use insertMany */
+
+    db.items.insertOne(doc);
+});
