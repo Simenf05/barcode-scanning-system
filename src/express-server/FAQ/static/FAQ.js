b375965f7b078436cf29b90e854fda50fde04e1e
Simenf05 <106535609+Simenf05@users.noreply.github.com>
2023-03-06 11:03:35 +0100

    fav icon and faq

--- src/express-server/FAQ/static/FAQ.js
+++ src/express-server/FAQ/static/FAQ.js
@@ -1,1 +1,25 @@
-console.log("simen");
+const boxEl = document.querySelector("#box")
+
+const questions = ["Why cant i log in?", "What tech is used for this website?", "Who made this website?"]
+
+const answers = ["You need a user with admin privileges.", "Express.js is used for this website.", "Simen Fritsvold made this website."]
+
+for (let i = 0; i < questions.length; i++) {
+
+    const newDiv = document.createElement("div")
+
+    const questionEl = document.createElement("p")
+    questionEl.innerText = questions[i]
+    questionEl.className = "question"
+    newDiv.appendChild(questionEl)
+
+    const answerEl = document.createElement("p")
+    answerEl.innerText = answers[i]
+    answerEl.className = "answer"
+    newDiv.appendChild(answerEl)
+
+    boxEl.appendChild(newDiv)
+
+}
+
+
