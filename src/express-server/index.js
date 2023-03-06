29d67949926bb1895ed59c683a366e8017c9294f
Simenf05 <106535609+Simenf05@users.noreply.github.com>
2023-03-06 09:46:38 +0100

    real favicon

--- src/express-server/index.js
+++ src/express-server/index.js
@@ -33,8 +33,8 @@ const server = app.listen(internal_port, () => {
 })
 
 process.on('SIGTERM', () => {
-    debug('SIGTERM signal received: closing HTTP server')
+    console.debug('SIGTERM signal received: closing HTTP server')
     server.close(() => {
-      debug('HTTP server closed')
+      console.debug('HTTP server closed')
     })
   })
